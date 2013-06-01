af37f08c1d8b00c6af85a691f7aac5154d8646b9
Andrew Heaford <ahphoshi@gmail.com>
2013-06-02 00:36:48 +0100

    Memoised branching
    Most computation occurred during branching. Memoising this gives a
    significant boost to speed at a negligible memory usage increase.

--- brainfuck.hs
+++ brainfuck.hs
@@ -4,11 +4,13 @@ import Data.Char (ord, chr)
 import Data.Array
 import qualified Data.Sequence as S (fromList, index, length, Seq, update, drop, reverse)
 data State = State {state :: S.Seq Char, pos :: Int} deriving (Show)
+type StateFuncArray = Array Integer State
+data JumpOptimiser = JumpOptimiser StateFuncArray StateFuncArray
 
 newState :: String -> State
 newState input = State (S.fromList input) 0
 dataState :: State
-dataState = State (S.fromList . take 5000 . repeat $ '\NUL') 0
+dataState = State (S.fromList . take 30000 . repeat $ '\NUL') 0
 getPosition :: State -> Int
 getPosition (State _ n) = n;
 getData :: State -> S.Seq Char
@@ -26,23 +28,32 @@ offsetStatePointer (State state pos) offset = State state (pos + offset)
 indexOutOfRange :: State -> Bool
 indexOutOfRange (State state pos) = pos >= S.length state
 
+openArray :: JumpOptimiser -> StateFuncArray
+openArray (JumpOptimiser _ open) = open
+closeArray :: JumpOptimiser -> StateFuncArray
+closeArray (JumpOptimiser close _) = close
+
 brainfuck :: String -> String -> String
-brainfuck input var = getOut . until (indexOutOfRange . getInput) bf $ (newState input, dataState, "", newState var)
+brainfuck input var = getOut . until (indexOutOfRange . getInput) (bf funcArray) $ (inputState, dataState, "", newState var)
 	where getOut (_, _, out, _) = reverse out;
 		  getStateData (_, state, _, _) = getData state;
-		  getInput (input, _, _, _) = input
+		  getInput (input, _, _, _) = input;
+		  inputState = newState input;
+		  inputStateData = getData inputState;
+		  inputLength = fromIntegral . S.length $ inputStateData;
+		  funcArray = JumpOptimiser (memoArray toCloseBracket inputStateData inputLength) (memoArray toOpenBracket inputStateData inputLength)
 
-bf :: (State, State, String, State) -> (State, State, String, State)
-bf (input, state, output, var)
+bf :: JumpOptimiser -> (State, State, String, State) -> (State, State, String, State)
+bf funcArray (input, state, output, var)
 	| command == '+' = (newInput, plus state, output, var)
 	| command == '-' = (newInput, minus state, output, var)
 	| command == '>' = (newInput, rightShift state, output, var)
 	| command == '<' = (newInput, leftShift state, output, var)
 	| command == '.' = (newInput, state, getCurrent state:output, var)
 	| command == ',' = (newInput, setCurrent state (getCurrent var), output, rightShift var)
-	| command == '[' = if getCurrent state == '\NUL' then (toCloseBracket input, state, output, var)
+	| command == '[' = if getCurrent state == '\NUL' then (m_toCloseBracket (closeArray funcArray) input, state, output, var)
 							else (newInput, state, output, var)
-	| command == ']' = if getCurrent state /= '\NUL' then (toOpenBracket input, state, output, var)
+	| command == ']' = if getCurrent state /= '\NUL' then (m_toOpenBracket (openArray funcArray) input, state, output, var)
 							else (newInput, state, output, var)
 	| otherwise = (newInput, state, output, var)
 	where 	command = getCurrent input;
@@ -57,6 +68,12 @@ rightShift state = offsetStatePointer state 1
 leftShift :: State -> State
 leftShift state = offsetStatePointer state (-1)
 
+m_toCloseBracket :: StateFuncArray -> State -> State
+m_toCloseBracket funcArray (State state pos) = (funcArray)!(fromIntegral pos)
+
+memoArray :: (State -> State) -> S.Seq Char -> Integer -> StateFuncArray
+memoArray func stateSeq len = listArray (0, len) [func (State stateSeq n) | n <- [0..]]
+
 toCloseBracket :: State -> State
 toCloseBracket (State dat pos) = State dat (toCloseBracket' (S.drop pos dat) pos 0)
 	where toCloseBracket' :: S.Seq Char -> Int -> Int -> Int;
@@ -70,6 +87,9 @@ toCloseBracket (State dat pos) = State dat (toCloseBracket' (S.drop pos dat) pos
 		  			cPos + 1;
 
 		  toCloseBracket' state cPos openBrackets = toCloseBracket' (S.drop 1 state) (cPos+1) openBrackets;
+
+m_toOpenBracket :: StateFuncArray -> State -> State
+m_toOpenBracket funcArray (State state pos) = (funcArray)!(fromIntegral pos)
 
 toOpenBracket :: State -> State
 toOpenBracket (State dat pos) = State dat (toOpenBracket' (S.drop ((S.length dat - pos) - 1) . S.reverse $ dat) pos 0)
